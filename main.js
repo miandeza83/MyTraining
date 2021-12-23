60a69c8b00f7112d853356c7b655ac194790ed67
errollgnargnar <errollgnargnar@gmail.com>
2021-12-23 13:47:02 -0600

    make sources and conditions conainer show and hide

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const windowButtons = document.getElementsByClassName('window-btn');
-const currentButton = '';
+
+const sourcesContainer = document.querySelector('.sources-container');
+const conditionsContainer = document.querySelector('.conditions-container');
+
+let currentButton = ''; ///
+
+//turns off all conatiners from view
+const containers = document.getElementsByClassName('container');
+for(let i = 0; i < containers.length; i++){
+  containers[i].style.display = 'none';
+}
 
 function turnAllOff () {
   for (i = 0; i < windowButtons.length; i++){
-    windowButtons[i].style.backgroundColor = 'green';
+    windowButtons[i].style.backgroundColor = 'transparent';
   }
-  }
+}
+
+///turns buttons on and off when clicked also sets the container to be worked on....
+for (let i = 0; i < windowButtons.length; i++) {
+  windowButtons[i].addEventListener('click', () => {
+    turnAllOff();
+    windowButtons[i].style.backgroundColor = 'blue';
+    currentButton = windowButtons[i].textContent;
+
+    setWindowView(currentButton);
+
+  });
+}
 
 
-  for (let i = 0; i < windowButtons.length; i++) {
-    windowButtons[i].addEventListener('click', () => {
-      turnAllOff();
-      windowButtons[i].style.backgroundColor = 'blue';
-    })
-  }
- 
